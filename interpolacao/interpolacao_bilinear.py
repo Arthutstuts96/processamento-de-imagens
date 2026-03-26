08c1d0735209a41753dfe99055815c0eab9b3f71
Emanuel <badaro.fonseca@mail.uft.edu.br>
2026-03-25 23:22:18 -0300

    Tenta implementar interpolacao bilinear

--- interpolacao/interpolacao_bilinear.py
+++ interpolacao/interpolacao_bilinear.py
@@ -1,8 +1,60 @@
+import math
+
+import numpy as np
 from numpy.typing import NDArray
 
 
 def ampliar_bilinear(matriz: NDArray):
-    pass
+    """
+    Amplia uma matriz NumPy usando a interpolação bilinear.
+    """
+    linhas, colunas = matriz.shape
+    nova_matriz = np.zeros((linhas * 2, colunas * 2), dtype=np.uint8)
 
-def ampliar_bilinear(matriz: NDArray):
-    pass
+    for i in range(linhas-1): 
+        for j in range(colunas-1):
+            pixel1 = matriz[i][j]
+            pixel2 = matriz[i][j + 1]
+            pixel3 = matriz[i + 1][j]
+            pixel4 = matriz[i + 1][j + 1]
+
+            _i = i * 2
+            _j = j * 2
+
+           # f(i,j) - O pixel original
+            nova_matriz[_i, _j] = pixel1 
+
+            # a - Média horizontal superior
+            nova_matriz[_i, _j + 1] = (pixel1 + pixel2) // 2
+            
+            # b - Média vertical esquerda
+            nova_matriz[_i + 1, _j] = (pixel1 + pixel3) // 2
+            
+            # c - Média central (dos 4 pixels)
+            nova_matriz[_i + 1, _j + 1] = (pixel1 + pixel2 + pixel3 + pixel4) // 4
+            
+            # d - Média vertical direita (entre p2 e p4)
+            nova_matriz[_i + 1, _j + 2] = (pixel2 + pixel4) // 2
+            
+            # e - Média horizontal inferior (entre p3 e p4)
+            nova_matriz[_i + 2, _j + 1] = (pixel3 + pixel4) // 2
+
+    return nova_matriz
+def reduzir_bilinear(matriz: NDArray):
+    """
+    Reduz uma matriz NumPy usando a interpolação bilinear.
+    """
+    linhas, colunas = matriz.shape
+    nova_matriz = np.zeros((linhas // 2, colunas // 2), dtype=np.uint8)
+
+    for i in range(0, linhas - 1, 2):
+        for j in range(0, colunas - 1, 2):
+            pixel1 = matriz[i][j]
+            pixel2 = matriz[i][j + 1]
+            pixel3 = matriz[i + 1][j]
+            pixel4 = matriz[i + 1][j + 1]
+
+            # Média dos 4 pixels
+            nova_matriz[i // 2, j // 2] = (pixel1 + pixel2 + pixel3 + pixel4) // 4
+
+    return nova_matriz
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PIL import Image
 from conversao import converter_para_preto_e_branco, imagem_para_matriz, salvar_matriz_como_imagem
 from interpolacao.interpolacao_vizinho_proximo import ampliar_vizinho_proximo, reduzir_vizinho_proximo
+from interpolacao.interpolacao_bilinear import ampliar_bilinear, reduzir_bilinear
 import os
 
 def main():
-    caminho = "imagens/mario.png"
-    caminho_pb = "imagens/preto_e_branco/mario.png"
+    caminho = "imagens/praiadowindows.jpg"
+    caminho_pb = "imagens/preto_e_branco/praiadowindows_pb.jpg"
 
     try:
         imagem = Image.open(caminho_pb)
@@ -18,9 +19,16 @@ def main():
     
     print(f"Dimensões da matriz: {matriz_pixels.shape}")
 
-    nova_imagem = ampliar_vizinho_proximo(matriz_pixels)
+    # nova_imagem = ampliar_vizinho_proximo(matriz_pixels)
     # nova_imagem = reduzir_vizinho_proximo(matriz_pixels)
-    salvar_matriz_como_imagem(nova_imagem, "aumento_vizinho_mario.jpg")
+    # salvar_matriz_como_imagem(nova_imagem, "aumento_vizinho_mario.jpg")
     
+    nova_imagem = ampliar_bilinear(matriz_pixels)
+    salvar_matriz_como_imagem(nova_imagem, "aumento_bilinear_praiadowindows.jpg")
+
+    nova_imagem = reduzir_bilinear(matriz_pixels)
+    salvar_matriz_como_imagem(nova_imagem, "reduzido_bilinear_praiadowindows.jpg")
+
+
 if __name__ == "__main__":
     main()
